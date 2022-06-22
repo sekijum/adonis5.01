550c7d012d46b4c85dc5739018321a05a344d81d
sekiguchi jumpei <77970444+Ninja1891@users.noreply.github.com>
2022-06-23 03:02:43 +0900

    fix:

--- app/Routes/Auth0.ts
+++ app/Routes/Auth0.ts
@@ -4,8 +4,6 @@ import RequestLinkage from 'App/Http/Requests/Auth/RequestLinkage'
 export const Auth0 = () => {
     Route.group(() => {
         Route.get('/connections', 'Auth0Controller.connections')
-        Route.post('/link', 'Auth0Controller.link').middleware('auth:api').validate(RequestLinkage)
-        Route.get('/unlink/:connectionId', 'Auth0Controller.unlink').middleware('auth:api')
         Route.post('/signin', 'Auth0Controller.signin').validate(RequestLinkage)
         Route.post('/signup', 'Auth0Controller.signup').validate(RequestLinkage)
     })
